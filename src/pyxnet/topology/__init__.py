f31aa34faa4397c7b4abaf4b36634880699e7e6e
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-01-09 09:32:56 +0000

    Add missing __init__ files

---
+++ src/pyxnet/topology/__init__.py
@@ -0,0 +1,8 @@
+"""
+==================================
+Pyxnet topology definition library
+==================================
+
+:Authors: - Florian Dupeyron <florian.dupeyron@mugcat.fr>
+:Date: January 2023
+"""
